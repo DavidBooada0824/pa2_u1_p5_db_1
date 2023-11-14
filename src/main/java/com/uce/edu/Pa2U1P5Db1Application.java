0473c08234d5cf55d0bb325ba8461586f150d2d5
DavidBooada0824 <davikbrboada@gmail.com>
2023-11-13 19:41:02 -0500

    Taller 5 IoC DI

--- src/main/java/com/uce/edu/Pa2U1P5Db1Application.java
+++ src/main/java/com/uce/edu/Pa2U1P5Db1Application.java
@@ -1,11 +1,19 @@
 package com.uce.edu;
 
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.uce.edu.ioc.di.Estudiante;
+
 @SpringBootApplication
 public class Pa2U1P5Db1Application implements CommandLineRunner {
+	
+	@Autowired
+	private Estudiante estudiante;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U1P5Db1Application.class, args);
@@ -14,7 +22,14 @@ public class Pa2U1P5Db1Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		System.out.println("Taller 4 ");
+		
+		this.estudiante.setApellido("Boada");
+		this.estudiante.setNombre("David");
+		this.estudiante.setCedula("1720030723");
+		this.estudiante.setSalario(new BigDecimal(100));
+		System.out.println(estudiante);
+		
+	
 	}
 
 }
