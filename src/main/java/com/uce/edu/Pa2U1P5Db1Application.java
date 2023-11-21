391f3554b0e6b4ecba69aa8f72dabd72e0763d63
DavidBooada0824 <davikbrboada@gmail.com>
2023-11-20 19:50:44 -0500

    Taller 8

--- src/main/java/com/uce/edu/Pa2U1P5Db1Application.java
+++ src/main/java/com/uce/edu/Pa2U1P5Db1Application.java
@@ -1,7 +1,5 @@
 package com.uce.edu;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,13 +10,19 @@ import com.uce.edu.service.IMateriaService;
 
 @SpringBootApplication
 public class Pa2U1P5Db1Application implements CommandLineRunner {
-	
+
 	@Autowired
 	private IMateriaService iMateriaService;
-	
+
 	@Autowired
 	private Materia materia;
-	
+
+	@Autowired
+	private Materia materia1;
+
+	@Autowired
+	private Materia materia2;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U1P5Db1Application.class, args);
 	}
@@ -26,35 +30,20 @@ public class Pa2U1P5Db1Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
+
+		this.materia.setNombre("Avanzada II");
+		System.out.println(this.materia);
+
+		this.materia1.setNombre("Nuevo Nmbre");
+		System.out.println(this.materia1);
+		System.out.println(this.materia);
 		
+		
+		this.materia2.setNombre("Nombre FInal");
+		System.out.println(this.materia2);
+		System.out.println(this.materia1);
+		System.out.println(this.materia);
 
-	
-	this.materia.setCodigo("M1");
-	this.materia.setNombre("P.AVANZADA");
-	this.materia.setNumeroCreditos(10);
-	
-	this.iMateriaService.registar(materia);
-
-	System.out.println("\nReporte Inicio");
-	List<Materia> reporte = this.iMateriaService.buscarTodo();
-	for(Materia mat:reporte) {
-		System.out.println(mat);;
-	}
-	System.out.println("\nReporte Fin\n");
-	Materia mate = this.iMateriaService.buscar("M1");
-	System.out.println(mate);
-	
-	mate.setNumeroCreditos(20);
-	this.iMateriaService.actualizar(mate);
-	Materia mate2 = this.iMateriaService.buscar("M1");
-	System.out.println(mate2);
-	
-	
-	this.iMateriaService.eliminar("M1");
-	mate2 = this.iMateriaService.buscar("M1");
-	System.out.println(mate2);
-
-	
 	}
 
 }
