81d8cf2cfac50ea48b849039c28df7d5e5d6a596
DavidBooada0824 <davikbrboada@gmail.com>
2023-11-20 19:25:45 -0500

    Taller4.2

--- src/main/java/com/uce/edu/Pa2U1P5Db1Application.java
+++ src/main/java/com/uce/edu/Pa2U1P5Db1Application.java
@@ -1,5 +1,7 @@
 package com.uce.edu;
 
+import javax.annotation.processing.SupportedSourceVersion;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,6 +17,9 @@ public class Pa2U1P5Db1Application implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("Taller 4 ");
-	}
+		
+		System.out.println("Taller 4.1");
+		System.out.println("Misma rama");
+		System.out.println("Taller 4.2");	}
 
 }
